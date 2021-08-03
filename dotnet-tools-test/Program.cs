c5d9ead6d8b800e71a128264ee862d1ba7e1dc09
Aleksandar Bayrev <aleksandarbayrev@gmail.com>
2021-08-03 22:41:52 +0300

    Add TimerUtils

---
+++ dotnet-tools-test/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace dotnet.tools.test
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            while (true)
+            {
+                Task.Run(() => TimerUtils.SetInterval(() => Console.WriteLine("SetInterval test - Hello World!"), TimeSpan.FromMilliseconds(1000)));
+                Task.Run(() => TimerUtils.SetTimeout(() => Console.WriteLine("SetTimeout test - Hello World!"), TimeSpan.FromMilliseconds(1000)));
+            }
+        }
+    }
+}
---
+++ dotnet-tools/TimerUtils.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace dotnet.tools
+{
+    public static class TimerUtils
+    {
+        public static async Task SetInterval(Action action, TimeSpan timeout)
+        {
+            action();
+            await Task.Delay(timeout);
+            await SetInterval(action, timeout);
+        }
+
+        public static async Task SetTimeout(Action action, TimeSpan timeout)
+        {
+            await Task.Delay(timeout);
+            action();
+        }
+    }
+}
