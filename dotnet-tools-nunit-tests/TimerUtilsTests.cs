18e057a9375410268217fbfb41e2824381f1dfa9
Aleksandar Bayrev <aleksandarbayrev@gmail.com>
2021-11-02 18:29:46 +0200

    Add tests

---
+++ dotnet-tools-nunit-tests/TimerUtilsTests.cs
@@ -0,0 +1,28 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Threading.Tasks;
+
+namespace dotnet.tools.nunit.tests
+{
+    [TestClass]
+    public class TimerUtilsTests
+    {
+        [TestMethod]
+        public async Task SetTimeoutTest()
+        {
+            var data = "NotSet";
+            Assert.AreEqual("NotSet", data);
+            await TimerUtils.SetTimeout(() => data = "test", TimeSpan.FromMilliseconds(1000));
+            Assert.AreEqual("test", data);
+        }
+        [TestMethod]
+        public async Task SetIntervalTest()
+        {
+            var count = 0;
+            Assert.AreEqual(0, count);
+            TimerUtils.SetInterval(() => count++, TimeSpan.FromMilliseconds(1000));
+            await Task.Delay(1001);
+            Assert.AreEqual(1, count);
+        }
+    }
+}
